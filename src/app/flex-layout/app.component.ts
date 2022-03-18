16edd4af1f7509f58b5a389e75de7e3ff3afa59a
DEV-COM <kwp@tlen.pl>
2022-03-18 10:16:34 +0100

    piątek 10:00

--- src/app/flex-layout/app.component.ts
+++ src/app/flex-layout/app.component.ts
@@ -2,7 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  styles: [``],
-  template:``
+  styles: [`
+  .parent {height: 100px}
+  .left {background-color: cyan;}
+  .right {background-color: red;}
+  `],
+  
+  template:`
+  <!-- <div class="parent" fxLayout="row" fxLayout.sm="column" fxLayout.xs="column"> -->
+  <div class="parent" fxLayout="row" fxLayout.lt-md="column">
+  <div fxFlex="30%" class="left">LEWY</div>
+  <div fxFlex="70%" class="right">PRAWY</div>
+  </div>
+  <!-- <div class="parent" fxLayout="column">
+  <div fxFlex="30%" class="left">LEWY</div>
+  <div fxFlex="70%" class="right">PRAWY</div>
+  </div> -->
+  `
 })
 export class AppComponent {}
